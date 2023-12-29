51cfb111a297da567d71b28281fd779edd4c29c6
saikat <tawhidulislam3482@gmail.com>
2023-12-29 18:09:25 +0600

    custom navber ready

--- src/Navber/Navber.jsx
+++ src/Navber/Navber.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { MdOutlineMenuOpen } from "react-icons/md";
 import { RxCross1 } from "react-icons/rx";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Navlinks from "./Navlinks";
 const Navber = () => {
 
   const [openMenu, setMenu] = useState(false);
-  const { user, isAdmin, logOut } = useState();
+//   const { user, isAdmin, logOut } = useState();
 
   const routes = [
     { path: "/", name: "Home" },
@@ -20,50 +20,29 @@ const Navber = () => {
   return (
     <nav className="flex justify-between items-center md:justify-center md:items-center  p-1 bg-gray-200 ">
       <div className="">
-        <div className="hidden dropdown">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              {user?.photoURL ? (
-                <img src={user?.photoURL} alt="" />
-              ) : (
-                <img src="" alt="" />
-              )}
-            </div>
-          </label>
-          <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ">
-            <li className="">
-              <button className="btn btn-sm uppercase">
-                {user?.displayName}
-              </button>
-            </li>
 
-            <li className="">
-              {user && isAdmin && (
-                <Link
-                  to={"/dashboard/adminHome"}
-                  className="btn btn-sm uppercase"
-                >
-                  Dashboard
-                </Link>
-              )}
-              {user && !isAdmin && (
-                <Link to={"/dashboard/home"} className="btn btn-sm uppercase">
-                  Dashboard
-                </Link>
-              )}
-            </li>
-
-            <li className="">
-              <button onClick={logOut} className="btn btn-sm uppercase">
-                Logout
-              </button>
-            </li>
-          </ul>
+      <div className="dropdown  ">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
+      </div>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+
       </div>
 
       <div
-        className="md:hidden mr-64 "
+        className="md:hidden mr-32 "
         onClick={() => setMenu(!openMenu)}
       >
         {openMenu === true ? (
@@ -74,7 +53,7 @@ const Navber = () => {
       </div>
 
       <ul
-        className={` md:flex  md:justify-center gap-8 rounded-md w-40  md:w-full duration-1000 absolute left-0 md:static  p-4
+        className={` md:flex  md:justify-center gap-8 rounded-md w-40  md:w-full duration-1000 absolute left-0 md:static  p-4 bg-gray-300
       ${openMenu ? "top-16" : "-top-60"}
       `}
       >

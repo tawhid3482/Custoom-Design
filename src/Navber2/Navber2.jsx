ad695ff703f23b9129c73288442e16d303e9a644
saikat <tawhidulislam3482@gmail.com>
2024-01-04 15:02:55 +0600

    course woirking

--- src/Navber2/Navber2.jsx
+++ src/Navber2/Navber2.jsx
@@ -1,13 +1,19 @@
 import RouteLink from "./RouteLink";
+import { RiMenuSearchLine } from "react-icons/ri";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 
 const Navber2 = () => {
+    const [open, setOpen]=useState(false)
   const routes = [
     { id: 1, path: "/", name: "Home" },
     { id: 2, path: "/about", name: "About" },
     { id: 3, path: "/service", name: "Service" },
+    { id: 3, path: "/service", name: "2" },
   ];
   return (
-    <nav className="bg-gray-300  p-4  my-5 rounded-lg">
+    <nav className="flex items-center justify-between bg-gray-300  p-4  my-5 rounded-lg">
+      <div className="">logo</div>
       <div className="">
         <ul className="flex items-center justify-center">
           {routes?.map((route) => (
@@ -16,6 +22,13 @@ const Navber2 = () => {
             </RouteLink>
           ))}
         </ul>
+      </div>
+      <div onClick={()=>setOpen(!open)} className="text-3xl">
+        {open === true ? (
+          <IoMdClose></IoMdClose>
+        ) : (
+          <RiMenuSearchLine className="text-3xl"></RiMenuSearchLine>
+        )}
       </div>
     </nav>
   );
--- src/Navber2/RouteLink.jsx
+++ src/Navber2/RouteLink.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 const RouteLink = ({route}) => {
     return (
         <div>
-            <li className="text-lg mr-16">
+            <li className="text-lg mr-16 hover:bg-red-300 p-2 rounded-lg">
                 <NavLink to={route.path}
                 className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? "p-2  rounded-lg text-center" : ""
+                isPending ? "pending" : isActive ? "p-2 bg-red-400 rounded-lg text-center" : ""
               }
                 >
 

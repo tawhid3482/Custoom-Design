219f05305e076e905c19eb3c098c2ac8d5231103
saikat <tawhidulislam3482@gmail.com>
2024-01-04 19:34:38 +0600

    navber eorking

--- src/Navber2/Navber2.jsx
+++ src/Navber2/Navber2.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 const Navber2 = () => {
     const [open, setOpen]=useState(false)
+
     const [openDropdown, setOpenDropdown] = useState(false); 
 
   const routes = [
@@ -22,7 +23,7 @@ const Navber2 = () => {
   };
   return (
     <nav className="flex items-center justify-between bg-gray-300  p-4  my-5 rounded-lg">
-      <div className="text-center md:static">logo</div>
+      <div className="">logo</div>
       <div className="">
         <ul className={`md:flex md:static bg-gray-300 absolute px-5
         ${open ? 'top-16':'hidden'}
@@ -41,14 +42,16 @@ const Navber2 = () => {
           <RiMenuSearchLine className="text-3xl"></RiMenuSearchLine>
         )}
       </div>
-      <div onClick={toggleDropdown} className="">
-      <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+      <div  className="">
+      <div onClick={toggleDropdown} className="dropdown dropdown-end">
+      <div  tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
       </div>
-      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className={`mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 ${
+              openDropdown ? "block" : "hidden"
+            } `}>
         <li onClick={closeDropdown}>
           <a className="justify-between">
             Profile
